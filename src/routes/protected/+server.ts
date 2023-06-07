75db548e60d5041c46d073d4cbb2f0c5572b21d7
ubermanu <manu@paca.happy-dev.fr>
2023-06-07 10:24:47 +0200

    minor change

--- src/routes/protected/+server.ts
+++ src/routes/protected/+server.ts
@@ -1,7 +1,9 @@
 import { redirect, type RequestHandler } from '@sveltejs/kit'
 
 export const GET: RequestHandler = async ({ locals }) => {
-  locals.session.set('flashMessage', {
+  const { session } = locals
+
+  session.set('flashMessage', {
     type: 'danger',
     message: 'You are not authorized to view this page',
   })
